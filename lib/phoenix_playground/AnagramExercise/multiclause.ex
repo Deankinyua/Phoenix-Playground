509249f999211eba7f38b50d546d4c3a841457f5
Dean Kinyua <kinyuadean@gmail.com>
2024-07-15 08:28:25 +0300

    back to Daraja

---
+++ lib/phoenix_playground/AnagramExercise/multiclause.ex
@@ -0,0 +1,69 @@
+defmodule PhoenixPlayground.Multiclause do
+  def fact(0), do: 1
+  def fact(n), do: n * fact(n - 1)
+
+  # * This is an Overloaded multiclause function which uses
+  # * pattern matching to find the area of different shapes
+  # *  A clause is a function definition specified by the def expression.
+
+  def calc_area({:rectangle, a, b}) do
+    a * b
+  end
+
+  def calc_area({:circle, a}) do
+    a * a * 3.14
+  end
+
+  def calc_area({:square, a}) do
+    a * a
+  end
+
+  # * Place the case that always matches last so the runtime is not confused
+
+  def calc_area(unknown) do
+    {:error, {:unknown_shape, unknown}}
+  end
+
+  # * Branching with MultiClause Functions
+  # * Branch based on the shape of the input data
+
+  def is_list_empty?([]), do: true
+  def is_list_empty?([_ | _]), do: false
+
+  # * behaves differently when called with a binary or a number and uses Guard Expressions
+
+  def double(x) when is_number(x), do: x * 2
+  def double(x) when is_binary(x), do: x <> x
+
+  def ret_atom(num) when is_number(num) and num < 0 do
+    :negative
+  end
+
+  def ret_atom(num) when num === 0 do
+    :zero
+  end
+
+  def ret_atom(num) when is_number(num) do
+    :positive
+  end
+
+  # * Implementing MultiClause Lambdas (Anonymous Functions)
+
+  def multiclause_lambda(x) do
+    test_num =
+      fn
+        x when is_number(x) and x < 0 -> :negative
+        x when x == 0 -> :zero
+        x when is_number(x) and x > 0 -> :positive
+      end
+
+    test_num.(x)
+  end
+
+  # * Returns the minimal element in the enumerable according to Erlang's term ordering.
+  def smallest(list) when length(list) > 0 do
+    Enum.min(list)
+  end
+
+  def smallest(_), do: {:error, :invalid_argument}
+end
--- lib/phoenix_playground/AnagramExercise/sender.ex
+++ lib/phoenix_playground/AnagramExercise/sender.ex
@@ -6,46 +6,6 @@ defmodule PhoenixPlayground.Sender do
     send(pid, {self(), "some_message"})
   end
 
-  # * This is an Overloaded multiclause function which uses
-  # * pattern matching to find the area of different shapes
-  # *  A clause is a function definition specified by the def expression.
-
-  def calc_area({:rectangle, a, b}) do
-    a * b
-  end
-
-  def calc_area({:circle, a}) do
-    a * a * 3.14
-  end
-
-  def calc_area({:square, a}) do
-    a * a
-  end
-
-  # * Place the case that always matches last so the runtime is not confused
-
-  def calc_area(unknown) do
-    {:error, {:unknown_shape, unknown}}
-  end
-
-  def ret_atom(num) when is_number(num) and num < 0 do
-    :negative
-  end
-
-  def ret_atom(num) when num === 0 do
-    :zero
-  end
-
-  def ret_atom(num) when is_number(num) do
-    :positive
-  end
-
-  def smallest(list) when length(list) > 0 do
-    Enum.min(list)
-  end
-
-  def smallest(_), do: {:error, :invalid_argument}
-
   # * The function determines if the bread has expired or not
 
   def isExpired(current_date, expiry_date) do
@@ -53,18 +13,5 @@ defmodule PhoenixPlayground.Sender do
       Date.compare(current_date, expiry_date) == :gt -> "The bread has expired"
       Date.compare(current_date, expiry_date) == :lt -> "The bread is okay for consumption"
     end
-  end
-
-  # * Implementing MultiClause Lambdas (Anonymous Functions)
-
-  def multiclause_lambda(x) do
-    test_num =
-      fn
-        x when is_number(x) and x < 0 -> :negative
-        x when x == 0 -> :zero
-        x when is_number(x) and x > 0 -> :positive
-      end
-
-    test_num.(x)
   end
 end
