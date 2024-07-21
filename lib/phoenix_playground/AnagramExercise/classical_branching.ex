92680f637c3073187bfadd252926f6a00c929fb8
Dean Kinyua <kinyuadean@gmail.com>
2024-07-21 17:00:59 +0300

    first commit

--- lib/phoenix_playground/AnagramExercise/classical_branching.ex
+++ lib/phoenix_playground/AnagramExercise/classical_branching.ex
@@ -1,4 +1,5 @@
 defmodule PhoenixPlayground.ClassicalBranching do
+  @spec call_status(any()) :: :ended | :pending | :started
   def call_status(call) do
     # * Use a map to store the call. Maps in elixir can be compared to objects in oop languages
     # * E.G call = %{started_at: nil, ended_at: nil}
@@ -10,6 +11,13 @@ defmodule PhoenixPlayground.ClassicalBranching do
       # * (true) is always satisfied, this effectively becomes the fallback clause that is
       # * invoked if none of the previously stated conditions in the cond expression are met.
       true -> :pending
+    end
+  end
+
+  def max(a, b) do
+    case a >= b do
+      true -> "correct"
+      false -> "incorrect"
     end
   end
 end
